d648a990acb4986eb8a896802d5f8753a2a91756
Gabriel Henrique Furtado Barbosa <gbrielf@gabriel-henrique-barbosa.tail9a2363.ts.net>
2026-04-27 18:29:01 -0300

    refactor: alterando nome dos métodos para melhorar a clareza

--- Lista/No.java
+++ Lista/No.java
@@ -2,12 +2,36 @@ package Lista;
 
 public class No {
     private No anterior;
-    private No posterior;
+    private No proximo;
     private Object elemento;
 
     public No(Object elemento){
         this.anterior = null;
-        this.posterior = null;
+        this.proximo = null;
+        this.elemento = elemento;
+    }
+
+    public No getPrevious(){
+        return anterior;
+    }
+
+    public No getNext(){
+        return proximo;
+    }
+
+    public Object getElement(){
+        return elemento;
+    }
+
+    public void setPrevious(No anterior){
+        this.anterior = anterior;
+    }
+
+    public void setNext(No proximo){
+        this.proximo = proximo;
+    }
+
+    public void setElement(Object elemento){
         this.elemento = elemento;
     }
 }
