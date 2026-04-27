b0cbef834bf1d2690a3ee8126844821b130cc2af
Gabriel Henrique Furtado Barbosa <gbrielf@gabriel-henrique-barbosa.tail9a2363.ts.net>
2026-04-27 18:30:00 -0300

    feat: adicionando os métodos da class ListaDuplamenteLigada estando em conformidade com a sua devida interface

--- Lista/Lista.java
+++ Lista/Lista.java
@@ -1,25 +1,28 @@
 package Lista;
 
-public interface Lista {
+public interface Lista{
     // métodos genéricos
-    public int size();
-    public boolean isEmpty();
+    public int size(); //ok
+    public boolean isEmpty(); //ok
+
     // métodos de fila
-    public boolean isFirst(No n);
-    public boolean isLast(No n);
+    public boolean isFirst(No n); //ok
+    public boolean isLast(No n); //ok
+
     // métodos de acesso a info
-    public Object first();
-    public Object last();
-    public Object before(Object p);
-    public Object after(Object p);
+    public Object getFirst(); //ok
+    public Object getLast(); //ok
+    public No before(No p); //ok  retorna nó antes
+    public No after(No p); //ok retorna nó seguinte
+
     // métodos para atualizar
-    public Object replaceElement(No n, Object o); // altera o conteúdo do nó
-    public Object swapElements(No n, No q); // troca a posição de dois nós
+    public Object replaceElement(No n, Object o); // ok. altera o conteúdo do nó
+    public void swapElements(No n, No q); // ok. troca a posição de dois nós
 
-    public void insertBefore(No n, Object o);
-    public void insertAfter(No n, Object o);
-    public void insertFirst(Object o);
-    public void insertLast(Object o);
+    public No insertBefore(No n, Object o); // ok insere antes do no n
+    public No insertAfter(No n, Object o); // ok inser depois do no n
+    public void insertFirst(Object o); // ok insere depois do head
+    public void insertLast(Object o); // ok insere depois do tail
 
-    public Object remove(No n);    
+    public Object remove(No n); // ok
 }
