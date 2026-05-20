5dfb7ee50f8f48155986b5f1fcb4a5916bf9b042
Gabriel Henrique F. Barbosa <56904537+gbrielf@users.noreply.github.com>
2026-05-20 18:03:06 +0000

    feat: tad sequencia implementado

--- Sequencia/Sequencia.java
+++ Sequencia/Sequencia.java
@@ -1,5 +1,7 @@
 package Sequencia;
 
+import Lista.No;
+
 public interface Sequencia{
     // métodos genéricos
     public int size();
@@ -10,18 +12,21 @@ public interface Sequencia{
     public void insertAtRank(int r, Object o);
     public Object removeAtRank(int r);
     // métodos de lista duplamente ligada
-    public No first();
-    public No last();
-    public No before();
-    public No after();
+    public No isFirst();
+    public No isLast();
+    public No before(No p);
+    public No after(No p);
+
     public Object replaceElement(int n, Object o);
     public void swapElements(int n, int q);
+
     public void insertBefore(int n, Object o);
     public void insertAfter(int n, Object o);
     public void insertFirst(Object o);
     public void insertLast(Object o);
+
     public Object remove(int n);
     // métodos ponte
     public Object atRank(int r);
-    public Object rankOf(int n);
+    public Object rankOf(No n);
 }
