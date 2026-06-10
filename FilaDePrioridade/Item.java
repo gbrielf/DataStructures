39884af5b5626b29624d37cc6df2c0090e9de0cf
Gabriel Henrique Furtado Barbosa <gbrielf@gabriel-henrique-barbosa.tail9a2363.ts.net>
2026-06-10 12:17:32 -0300

    feat: salvando progresso fila de prioridade com heap arvore binaria

--- FilaDePrioridade/Item.java
+++ FilaDePrioridade/Item.java
@@ -1,17 +1,21 @@
 public class Item<T>{
     private int chave;
-    private Object elemento;  
+    private T elemento;  
     
     public Item(int chave, T elemento){
         this.elemento = elemento;   
         this.chave = chave;
     }
 
-    public int getChave(){
+    public int getKey(){
         return this.chave;
     }
 
     public T getElemento(){
         return this.elemento;
+    }
+
+    public void setElemento(T elemento){
+        this.elemento = elemento;
     }
 }
