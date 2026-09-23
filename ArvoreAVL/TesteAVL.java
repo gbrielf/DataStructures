a728bf99df147e57ee54d51d96279266dd904d02
Gabriel Henrique Furtado Barbosa <gabrielhfurtadobarbosa@gmail.com>
2026-09-23 16:41:06 -0300

    refactor: quebra de linha das  mensagens impressas no terminal

--- ArvoreAVL/TesteAVL.java
+++ ArvoreAVL/TesteAVL.java
@@ -28,14 +28,14 @@ public class TesteAVL {
             System.out.println("5. Mostrar altura da árvore");
             System.out.println("6. Buscar valor de um nó");
             System.out.println("0. Sair");
-            System.out.print("Escola uma opção e\naperte na tecla '0'\npara finalizar o programa.\n\nDigite a opção desejada:");
+            System.out.print("Escola uma opção e\naperte na tecla '0'\npara finalizar o programa.\n\nDigite a opção desejada:\n");
 
             String opcao = scanner.nextLine().trim();
 
             switch(opcao) {
                 case "1":
                     try{
-                        System.out.print("Digite um valor inteiro para inserir na árvore:");
+                        System.out.print("Digite um valor inteiro para inserir na árvore:\n");
                         int novoValor = Integer.parseInt(scanner.nextLine().trim());
                         System.out.println("Digite o elemento associado ao valor (ou deixe em branco para null):");
                         String elementoInput = scanner.nextLine().trim();
@@ -87,12 +87,13 @@ public class TesteAVL {
                     } catch (NumberFormatException e) {
                         System.out.println("Erro: Valor inválido! Digite um número inteiro.");
                     } catch (Exception e) {
-                        System.out.println("Error: " + e.getMessage());
+                        System.out.println("Error: " + e.getMessage()+ "");
                     }
                     break;
                 case "4":
-                    System.out.println("Árvore AVL:");
+                    System.out.println("\nÁrvore AVL atual:");
                     arvore.printTree();
+                    System.out.println("\n");
                     break;
 
                 case "5":
@@ -105,7 +106,7 @@ public class TesteAVL {
                         int valorABuscar = Integer.parseInt(scanner.nextLine().trim());
                         NoAVL<Object> itemEncontrado = (NoAVL<Object>) arvore.search(valorABuscar);
                         if(itemEncontrado != null){
-                            System.out.println("O nó com valor " + valorABuscar + " possui o elemento " + itemEncontrado.getItem().getElemento() + ".");
+                            System.out.println("O nó com valor " + valorABuscar + " possui o elemento " + itemEncontrado.getItem().getElemento());
                         } else {
                             System.out.println("O nó com valor " + valorABuscar + " não foi encontrado na árvore.");
                         }
